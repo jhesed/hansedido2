69e67b5436c27774e948d4a150809ec811718dce
jhesed <jhesed.tacadena@gmail.com>
2019-11-03 06:42:10 +0800

    color-change: Working rsvp database update

---
+++ _include/php/db_config.php
@@ -0,0 +1,9 @@
+<?php
+
+	define( 'DB_NAME', 'jhesedhannah' );
+	define( 'DB_USER', 'jhesedhannah' );
+	define( 'DB_PASSWORD', '12345jhesed' );
+	define( 'DB_HOST', 'localhost' );
+
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+?>
--- _include/php/rsvp.php
+++ _include/php/rsvp.php
@@ -1,51 +1,55 @@
 <?php
 
-function rsvp_submission()
-{
-    /* Handles RSVP reservation logic */
+require "db_config.php";
 
-    // Initialization
-    $table_name = "jh_guests";
-    $response = array(
-        'error' => false
-    );
-    $data = array();
+/* Handles RSVP reservation logic */
 
-    // Retrieve parameters
-    $data['first_name'] = strtolower(trim($_POST['first-name']));
-    $data['last_name'] = strtolower(trim($_POST['last-name']));
-    $data['message'] = trim($_POST['message']);
-    $data['attendance'] = intval($_POST["attendance"]);
-    $response['attendance'] = $data['attendance'];
+// Initialization
+$table_name = "jh_guests";
+$response = array(
+    'error' => false
+);
+$data = array();
 
-    // Basic validation
-    if ($data['first_name'] == '' || $data['last_name'] == '') {
-        $data['error'] = true;
-        exit(json_encode($data));
-    }
+// Retrieve parameters
+$data['first_name'] = strtolower(trim($_POST['first-name']));
+$data['last_name'] = strtolower(trim($_POST['last-name']));
+$data['message'] = trim($_POST['message']);
+$data['attendance'] = intval($_POST["attendance"]);
+$response['attendance'] = $data['attendance'];
 
-    // Data validation
-    $query = "SELECT * FROM $table_name WHERE first_name = '" . $data["first_name"] . "' and last_name = '" . $data["last_name"] . "'";
-    $result = $wpdb->get_row($query);
+// Basic validation
+if ($data['first_name'] == '' || $data['last_name'] == '') {
+    $data['error'] = true;
+    exit(json_encode($data));
+}
 
-    if ($result == null) {
-        $response['error'] = true;
-        $response['ecode'] = 'NOT_FOUND';
-        exit(json_encode($response));
-    }
+// Data validation
+$query = "SELECT * FROM $table_name WHERE first_name = '" . $data["first_name"] . "' and last_name = '" . $data["last_name"] . "'";
+$result = $mysqli->query($query)->fetch_object();
 
-    // Update attendance value
-    $update_query = "UPDATE $table_name SET attendance = " . $data["attendance"] . ", message = '" . $data["message"] . "' WHERE id = %d";
-    $wpdb->query($wpdb->prepare($update_query, $result->id));
-
-    // Send confirmation email
-    $message = file_get_contents(get_template_directory() . '/emails/rvsp/confirmation.html');
-
-    mail($result->email, "[han-sed-i-do] Attendance Confirmation", $message);
-
+if ($result == null) {
+    $response['error'] = true;
+    $response['ecode'] = 'NOT_FOUND';
     exit(json_encode($response));
 }
 
-rsvp_submission();
+// Update attendance value
+$update_query = "UPDATE $table_name SET attendance = " . $data["attendance"] . ", message = '" . $data["message"] . "' WHERE id = " . $result->id;
+
+$mysqli->query($update_query);    
+
+// Send confirmation email
+$headers = "From: " . strip_tags("rsvp@hansedido.com") . "\r\n";
+$headers .= "Reply-To: ". strip_tags("rsvp@hansedido.com") . "\r\n";
+$headers .= "CC: jhesed.tacadena@gmail.com\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$subject = "[han-sed-i-do] Attendance Confirmation";
+$message = file_get_contents('../../emails/rsvp/confirmation.html');
+
+mail($result->email, $subject, $message, $headers);
+
+exit(json_encode($response));
 
 ?>
