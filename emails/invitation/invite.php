ad7b0462bf2505fd69e4a76e11d798776f9ae1d3
jhesed <jhesed.tacadena@gmail.com>
2020-01-04 06:40:29 +0800

    Added text facility

--- emails/invitation/invite.php
+++ emails/invitation/invite.php
@@ -1,213 +1,229 @@
 <?php
-	$to      = 'tanlangcohannah@gmail.com';
-	$user 	 = 'Hannah';
+
+	$file = fopen("../guest_list.csv","r");
 	$subject = "You're invited!";
-	$message = <<<EOD
-		<!DOCTYPE html>
-		<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"
-			style="margin: 0 auto !important; padding: 0 !important; height: 100% !important; width: 100% !important; background: #f1f1f1;"
-		>
-		<head>
-		    <meta charset="utf-8"> <!-- utf-8 works for most cases -->
-		    <meta name="viewport" content="width=device-width"> <!-- Forcing initial-scale shouldn't be necessary -->
-		    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
-		    <meta name="x-apple-disable-message-reformatting">  <!-- Disable auto-scale in iOS 10 Mail entirely -->
-		    <title></title> <!-- The title tag shows in email notifications, like Android 4.4. -->
 
-		    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900" rel="stylesheet">
+	echo("Sending to:");	
+	while(! feof($file)) {
+		$data = fgetcsv($file);
+		$to      = $data[3];
+		$user 	 = $data[1];
 
-		    <!-- CSS Reset : BEGIN -->
-		    <style>
-
-		        /* What it does: Remove spaces around the email design added by some email clients. */
-		        /* Beware: It can remove the padding / margin and add a background color to the compose a reply window. */
-
-		/* What it does: Stops email clients resizing small text. */
-		* {
-		    -ms-text-size-adjust: 100%;
-		    -webkit-text-size-adjust: 100%;
+		if(empty($to)) {
+			continue;
 		}
 
-		/* What it does: Centers email on Android 4.4 */
-		div[style*="margin: 16px 0"] {
-		    margin: 0 !important;
-		}
+		$message = <<<EOD
+			<!DOCTYPE html>
+			<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"
+				style="margin: 0 auto !important; padding: 0 !important; height: 100% !important; width: 100% !important; background: #f1f1f1;"
+			>
+			<head>
+			    <meta charset="utf-8"> <!-- utf-8 works for most cases -->
+			    <meta name="viewport" content="width=device-width"> <!-- Forcing initial-scale shouldn't be necessary -->
+			    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
+			    <meta name="x-apple-disable-message-reformatting">  <!-- Disable auto-scale in iOS 10 Mail entirely -->
+			    <title></title> <!-- The title tag shows in email notifications, like Android 4.4. -->
 
-		/* What it does: Stops Outlook from adding extra spacing to tables. */
-		table,
-		td {
-		    mso-table-lspace: 0pt !important;
-		    mso-table-rspace: 0pt !important;
-		}
+			    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900" rel="stylesheet">
 
-		/* What it does: Fixes webkit padding issue. */
-		table {
-		    border-spacing: 0 !important;
-		    border-collapse: collapse !important;
-		    table-layout: fixed !important;
-		    margin: 0 auto !important;
-		}
+			    <!-- CSS Reset : BEGIN -->
+			    <style>
 
-		/* What it does: Uses a better rendering method when resizing images in IE. */
-		img {
-		    -ms-interpolation-mode:bicubic;
-		}
+			        /* What it does: Remove spaces around the email design added by some email clients. */
+			        /* Beware: It can remove the padding / margin and add a background color to the compose a reply window. */
 
-		/* What it does: Prevents Windows 10 Mail from underlining links despite inline CSS. Styles for underlined links should be inline. */
+			/* What it does: Stops email clients resizing small text. */
+			* {
+			    -ms-text-size-adjust: 100%;
+			    -webkit-text-size-adjust: 100%;
+			}
 
-		/* What it does: A work-around for email clients meddling in triggered links. */
-		*[x-apple-data-detectors],  /* iOS */
-		.unstyle-auto-detected-links *,
-		.aBn {
-		    border-bottom: 0 !important;
-		    cursor: default !important;
-		    color: inherit !important;
-		    text-decoration: none !important;
-		    font-size: inherit !important;
-		    font-family: inherit !important;
-		    font-weight: inherit !important;
-		    line-height: inherit !important;
-		}
+			/* What it does: Centers email on Android 4.4 */
+			div[style*="margin: 16px 0"] {
+			    margin: 0 !important;
+			}
 
-		/* What it does: Prevents Gmail from displaying a download button on large, non-linked images. */
-		.a6S {
-		    display: none !important;
-		    opacity: 0.01 !important;
-		}
+			/* What it does: Stops Outlook from adding extra spacing to tables. */
+			table,
+			td {
+			    mso-table-lspace: 0pt !important;
+			    mso-table-rspace: 0pt !important;
+			}
 
-		/* What it does: Prevents Gmail from changing the text color in conversation threads. */
-		.im {
-		    color: inherit !important;
-		}
+			/* What it does: Fixes webkit padding issue. */
+			table {
+			    border-spacing: 0 !important;
+			    border-collapse: collapse !important;
+			    table-layout: fixed !important;
+			    margin: 0 auto !important;
+			}
 
-		/* If the above doesn't work, add a .g-img class to any image in question. */
-		img.g-img + div {
-		    display: none !important;
-		}
+			/* What it does: Uses a better rendering method when resizing images in IE. */
+			img {
+			    -ms-interpolation-mode:bicubic;
+			}
 
-		/* What it does: Removes right gutter in Gmail iOS app: https://github.com/TedGoas/Cerberus/issues/89  */
-		/* Create one of these media queries for each additional viewport size you'd like to fix */
+			/* What it does: Prevents Windows 10 Mail from underlining links despite inline CSS. Styles for underlined links should be inline. */
 
-		/* iPhone 4, 4S, 5, 5S, 5C, and 5SE */
-		@media only screen and (min-device-width: 320px) and (max-device-width: 374px) {
-		    u ~ div .email-container {
-		        min-width: 320px !important;
-		    }
-		}
-		/* iPhone 6, 6S, 7, 8, and X */
-		@media only screen and (min-device-width: 375px) and (max-device-width: 413px) {
-		    u ~ div .email-container {
-		        min-width: 375px !important;
-		    }
-		}
-		/* iPhone 6+, 7+, and 8+ */
-		@media only screen and (min-device-width: 414px) {
-		    u ~ div .email-container {
-		        min-width: 414px !important;
-		    }
-		}
+			/* What it does: A work-around for email clients meddling in triggered links. */
+			*[x-apple-data-detectors],  /* iOS */
+			.unstyle-auto-detected-links *,
+			.aBn {
+			    border-bottom: 0 !important;
+			    cursor: default !important;
+			    color: inherit !important;
+			    text-decoration: none !important;
+			    font-size: inherit !important;
+			    font-family: inherit !important;
+			    font-weight: inherit !important;
+			    line-height: inherit !important;
+			}
+
+			/* What it does: Prevents Gmail from displaying a download button on large, non-linked images. */
+			.a6S {
+			    display: none !important;
+			    opacity: 0.01 !important;
+			}
+
+			/* What it does: Prevents Gmail from changing the text color in conversation threads. */
+			.im {
+			    color: inherit !important;
+			}
+
+			/* If the above doesn't work, add a .g-img class to any image in question. */
+			img.g-img + div {
+			    display: none !important;
+			}
+
+			/* What it does: Removes right gutter in Gmail iOS app: https://github.com/TedGoas/Cerberus/issues/89  */
+			/* Create one of these media queries for each additional viewport size you'd like to fix */
+
+			/* iPhone 4, 4S, 5, 5S, 5C, and 5SE */
+			@media only screen and (min-device-width: 320px) and (max-device-width: 374px) {
+			    u ~ div .email-container {
+			        min-width: 320px !important;
+			    }
+			}
+			/* iPhone 6, 6S, 7, 8, and X */
+			@media only screen and (min-device-width: 375px) and (max-device-width: 413px) {
+			    u ~ div .email-container {
+			        min-width: 375px !important;
+			    }
+			}
+			/* iPhone 6+, 7+, and 8+ */
+			@media only screen and (min-device-width: 414px) {
+			    u ~ div .email-container {
+			        min-width: 414px !important;
+			    }
+			}
 
 
-		    </style>
+			    </style>
 
-		    <!-- CSS Reset : END -->
+			    <!-- CSS Reset : END -->
 
-		    <!-- Progressive Enhancements : BEGIN -->
-		    <style>
+			    <!-- Progressive Enhancements : BEGIN -->
+			    <style>
 
-			    .primary{
-			background: #f5564e;
-		}
+				    .primary{
+				background: #f5564e;
+			}
 
-		    </style>
+			    </style>
 
 
-		</head>
+			</head>
 
-		<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #222222;  margin: 0 auto !important; padding: 0 !important; height: 100% !important; width: 100% !important; background: #f1f1f1; 	font-family: 'Nunito Sans', sans-serif;	font-weight: 400; font-size: 15px; line-height: 1.8; color: rgba(0,0,0,.4);">
-			<center style="width: 100%; background-color: #f1f1f1;">
-		    <div style="display: none; font-size: 1px;max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
-		      &zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
-		    </div>
-		    <div style="max-width: 600px; margin: 0 auto;" class="email-container">
-		    	<!-- BEGIN BODY -->
-		      <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="   border-spacing: 0 !important; border-collapse: collapse !important; table-layout: fixed !important; margin: 0 auto !important; margin: auto;">
-		      	<tr>
-		          <td valign="top" style="padding: 1em 2.5em; background: #ffffff;">
-		          	<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-spacing: 0 !important; border-collapse: collapse !important; table-layout: fixed !important; margin: 0 auto !important;">
-		          		<tr>
-		          			<td width="40%" class="logo" style="text-align: left;">
-					            <h1 style="margin: 0; color: #000;	font-size: 20px; font-weight: 700; text-transform: uppercase; font-family: 'Nunito Sans', sans-serif; font-family: 'Nunito Sans', sans-serif; color: #000000;	margin-top: 0;"><a style="text-decoration: none; color: #000; font-size: 20px; font-weight: 700; text-transform: uppercase; font-family: 'Nunito Sans', sans-serif; color: #f5564e;" href="https://hansedido.com">Han Sed I Do</a></h1>
-					          </td>
-					          <td width="60%" class="logo" style="text-align: right;">
-					            <ul style="padding:0">
-					            	<li style="	list-style: none; display: inline-block; margin-left: 5px; font-size: 12px;	font-weight: 700; text-transform: uppercase;"><a href="https://hansedido.com" style="text-decoration: none; color: rgba(0,0,0,.6);">Home</a></li>
-					            </ul>
-					          </td>
-		          		</tr>
-		          	</table>
-		          </td>
-			      </tr><!-- end tr -->
-						<tr>
-		          <td valign="middle" class="hero" style="position: relative; background-image: url('https://hansedido.com/emails/rsvp/images/monogram.png'); background-size: cover; height: 600px;">
-		 
-		          </td>
-			      </tr><!-- end tr -->
-			      <tr>
-			        <td valign="middle" style="padding:2.5em; text-align:center; background: #800000">
-			        	<div class="heading-section heading-section-white">
-			          	<h2 style="color: #000000; font-size: 24px; margin-top: 0; line-height: 1.4; font-weight: 700; font-family: 'Nunito Sans', sans-serif; color: #000000;	margin-top: 0; color:white;">Invitation</h2>
-			          	</div>
-			        </td>
-			       <tr>
-			        <td style="text-align:center; background: #fafafa;">
-			          	<p>Dear <b><i>{USER}</i></b>,</p>
-			          	<p>You have a special place in our heart and we humbly invite you to be a part of our wedding.</p>
-			          	<p>Your presence will be a blessing.  </p>
-			          	<p>Please confirm your attendance <u>before</u> <b>January 1, 2020</b> via our website: <a href="https://hansedido.com" style="color: #f5564e">www.hansedido.com</a></p>
-			          	<p>See you!</p>
-			          	<p>With love,</p>
-			          	<p><i>Hannah and Jhesed</i></p>
-			        	</div>
-			        </td>
-			    </tr>
-			      </tr><!-- end: tr -->
-			      <tr>      
-					<td valign="middle" class="footer" style="padding:2.5em; background: #340404; color: rgba(255,255,255,.5);">
-		        		<table style="border-spacing: 0 !important; border-collapse: collapse !important; table-layout: fixed !important; margin: 0 auto !important;">
-		            	<tr>
-		                <td valign="top" width="33.333%">
-		                  <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
-		                    <tr>
-		                      <td style="text-align: left; padding-right: 10px;">
-		                      	<p><center>&copy; 2019 hansedido. All Rights Reserved</center></p>
-		                      </td>
-		                    </tr>
-		                  </table>
-		                </td>
-		              </tr>
-		            </table>
-		        	</td>
-		        </tr>
-		      </table>
+			<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #222222;  margin: 0 auto !important; padding: 0 !important; height: 100% !important; width: 100% !important; background: #f1f1f1; 	font-family: 'Nunito Sans', sans-serif;	font-weight: 400; font-size: 15px; line-height: 1.8; color: rgba(0,0,0,.4);">
+				<center style="width: 100%; background-color: #f1f1f1;">
+			    <div style="display: none; font-size: 1px;max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
+			      &zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+			    </div>
+			    <div style="max-width: 600px; margin: 0 auto;" class="email-container">
+			    	<!-- BEGIN BODY -->
+			      <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="   border-spacing: 0 !important; border-collapse: collapse !important; table-layout: fixed !important; margin: 0 auto !important; margin: auto;">
+			      	<tr>
+			          <td valign="top" style="padding: 1em 2.5em; background: #ffffff;">
+			          	<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-spacing: 0 !important; border-collapse: collapse !important; table-layout: fixed !important; margin: 0 auto !important;">
+			          		<tr>
+			          			<td width="40%" class="logo" style="text-align: left;">
+						            <h1 style="margin: 0; color: #000;	font-size: 20px; font-weight: 700; text-transform: uppercase; font-family: 'Nunito Sans', sans-serif; font-family: 'Nunito Sans', sans-serif; color: #000000;	margin-top: 0;"><a style="text-decoration: none; color: #000; font-size: 20px; font-weight: 700; text-transform: uppercase; font-family: 'Nunito Sans', sans-serif; color: #f5564e;" href="https://hansedido.com">Han Sed I Do</a></h1>
+						          </td>
+						          <td width="60%" class="logo" style="text-align: right;">
+						            <ul style="padding:0">
+						            	<li style="	list-style: none; display: inline-block; margin-left: 5px; font-size: 12px;	font-weight: 700; text-transform: uppercase;"><a href="https://hansedido.com" style="text-decoration: none; color: rgba(0,0,0,.6);">Home</a></li>
+						            </ul>
+						          </td>
+			          		</tr>
+			          	</table>
+			          </td>
+				      </tr><!-- end tr -->
+							<tr>
+     				  <td valign="middle" class="hero" style="position: relative; background-image: url('https://hansedido.com/emails/rsvp/images/monogram.png'); background-size: cover; height: 600px;">
+			 
+			          </td>
+				      </tr><!-- end tr -->
+				      <tr>
+				        <td valign="middle" style="padding:2.5em; text-align:center; background: #800000">
+				        	<div class="heading-section heading-section-white">
+				          	<h2 style="color: #000000; font-size: 24px; margin-top: 0; line-height: 1.4; font-weight: 700; font-family: 'Nunito Sans', sans-serif; color: #000000;	margin-top: 0; color:white;">Invitation</h2>
+				          	</div>
+				        </td>
+				       <tr>
+				        <td style="text-align:center; background: #fafafa;">
+				          	<p>Dear <b><i>{USER}</i></b>,</p>
+				          	<p>You have a special place in our heart and we humbly invite you to be a part of our wedding.</p>
+				          	<p>Your presence will be a blessing.  </p>
+				          	<p>Please confirm your attendance <u>before</u> <b>January 18, 2020</b> via our website: <a href="https://hansedido.com" style="color: #f5564e">www.hansedido.com</a></p>
+				          	<p>See you!</p>
+				          	<p>With love,</p>
+				          	<p><i>Hannah and Jhesed</i></p>
+				        	</div>
+				        </td>
+				    </tr>
+				      </tr><!-- end: tr -->
+				      <tr>      
+						<td valign="middle" class="footer" style="padding:2.5em; background: #340404; color: rgba(255,255,255,.5);">
+			        		<table style="border-spacing: 0 !important; border-collapse: collapse !important; table-layout: fixed !important; margin: 0 auto !important;">
+			            	<tr>
+			                <td valign="top" width="33.333%">
+			                  <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+			                    <tr>
+			                      <td style="text-align: left; padding-right: 10px;">
+			                      	<p><center>&copy; 2020 hansedido. All Rights Reserved</center></p>
+			                      </td>
+			                    </tr>
+			                  </table>
+			                </td>
+			              </tr>
+			            </table>
+			        	</td>
+			        </tr>
+			      </table>
 
-		    </div>
-		  </center>
-		</body>
-		</html>
+			    </div>
+			  </center>
+			</body>
+			</html>
 EOD;
 
 
-$message = str_replace('{USER}', ucfirst($user), $message);
+		$message = str_replace('{USER}', ucfirst($user), $message);
 
-$headers = "From: " . strip_tags("rsvp@hansedido.com") . "\r\n";
-$headers .= "Reply-To: ". strip_tags("rsvp@hansedido.com") . "\r\n";
-$headers .= "CC: jhesed.tacadena@gmail.com\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-$subject = "[han-sed-i-do] You are Invited";
+		$headers = "From: " . strip_tags("rsvp@hansedido.com") . "\r\n";
+		$headers .= "Reply-To: ". strip_tags("rsvp@hansedido.com") . "\r\n";
+		// $headers .= "CC: jhesed.tacadena@gmail.com\r\n";
+		$headers .= "MIME-Version: 1.0\r\n";
+		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+		$subject = "[han-sed-i-do] You are Invited";
 
-mail($to, $subject, $message, $headers);
+		echo("<br/>- ");
+		echo($to);
+		echo("<br/>");
+		mail($to, $subject, $message, $headers);
+	}
 
-echo $message;
+fclose($file);
+	// echo $message;
 ?>
---
+++ emails/invitation/mobile_invite.php
@@ -0,0 +1,25 @@
+<?php
+	require "../../_include/php/itextmo.php";
+	
+	$file = fopen("../guest_list.csv","r");
+
+	echo("Texting:");	
+	while(! feof($file)) {
+		$data = fgetcsv($file);
+		$to      = $data[4];
+		$user 	 = $data[1];
+
+		if(empty($to)) {
+			continue;
+		}
+
+		$message = "{USER}, You are invited to our wedding on February 25. Please confirm attendance via our website, https://hansedido.com before January 18. Thanks! - Jhesed and Hannah";
+		$message = str_replace('{USER}', ucfirst($user), $message);
+		echo("<br/> -");
+		echo($to);
+		// itexmo_curless($to, $message);
+    	itexmo_curl($to, $message);
+	}
+
+fclose($file);
+?>
